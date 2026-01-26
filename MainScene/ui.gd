f03b79f9881d608b7f2a4c770280afb0991d3196
SimoneMicillo <simone.napoles@gmail.com>
2026-01-26 17:13:05 +0100

    Sanity system
    Implementato fino ad ora:
    
    Movimento giocatore (Solo orizzontale)
    Movimento camera (Mouse)
    Toggle maschera ON/OFF
    Sanity system (Sanity scende se maschera ON)
    Game Over screen (retry, quit buttons)

--- MainScene/ui.gd
+++ MainScene/ui.gd
@@ -1,12 +1,29 @@
 extends CanvasLayer
 
-@onready var player: CharacterBody3D = $"../Player"
+@onready var player: Player = $"../Player"
 @onready var playerMask : MaskComponent = $"../Player".get_node("MaskComponent")
 
 func _ready() -> void:
-	player.get_node("MaskComponent").sanityChanged.connect(updateSanityUI)
+	playerMask.sanityChanged.connect(updateSanityUI)
+	player.dead.connect(setGameOver)
 
 func updateSanityUI(nVal : float) -> void:
-	if nVal >= playerMask.sanity:
-		
 	$sanity.text = "Sanity = " + str(nVal)
+	if nVal >= playerMask.max_sanity:
+		get_tree().create_tween().tween_property($sanity, "modulate", Color(1.0, 1.0, 1.0, 0.0), .25)
+	elif nVal < playerMask.max_sanity:
+		if nVal <= playerMask.max_sanity / 3:
+			get_tree().create_tween().tween_property($sanity, "modulate", Color(1.0, 0.0, 0.0, 1.0), .25)
+		elif nVal <= playerMask.max_sanity / 2:
+			get_tree().create_tween().tween_property($sanity, "modulate", Color(1.0, 1.0, 0.0, 1.0), .25)
+		else:
+			get_tree().create_tween().tween_property($sanity, "modulate", Color(1.0, 1.0, 1.0, 1.0), .25)
+
+func _on_retry_pressed() -> void:
+	get_tree().reload_current_scene()
+func _on_quit_pressed() -> void:
+	get_tree().quit()
+
+func setGameOver() -> void:
+	$GameOver.show()
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
--- Player/mask_component.gd
+++ Player/mask_component.gd
@@ -7,22 +7,30 @@ signal sanityChanged(nSanity : float)
 var isMaskActive : bool = false
 
 @export_category("Sanity Stats")
-
 @export var max_sanity : float = 100.0
 var sanity : float
-@export var reduceSanityAmount : float = 10.0
+@export var reduceSanityAmount : float = 1.0
 
 func _ready() -> void:
 	sanity = max_sanity
 
 func setMaskActive() -> void:
 	isMaskActive = true
-	timer.start()
-
 func setMaskInactive() -> void:
 	isMaskActive = false
-	timer.stop()
 
+@onready var player: Player = $".."
 func _on_timer_timeout() -> void:
-	sanity -= reduceSanityAmount
+	if isMaskActive:
+		if sanity > 0:
+			sanity -= reduceSanityAmount
+		elif sanity <= 0:
+			player.setDead(true) #BUONO 
+			
+			#OPPURE ALTRO METODO
+			#get_parent().setDead(true) #Get_parent prende il padre ("Player") BRUTTO
+	#Gestione incremento sanity SE player non ha maschera ON
+	#else:
+		#if sanity < 100:
+			#sanity += reduceSanityAmount
 	sanityChanged.emit(sanity)
