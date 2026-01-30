43e57ae03951c200341744f84bc225c93a0a3dfc
SimoneMicillo <simone.napoles@gmail.com>
2026-01-30 22:42:14 +0100

    a

--- Scenary/mappa.gd
+++ Scenary/mappa.gd
@@ -1,10 +1,12 @@
 extends Node3D
 
-@onready var ui: CanvasLayer = $"../UI"
+@onready var hud: CanvasLayer = $"../UI"
 
 func _on_player_collider_body_entered(_body: Node3D) -> void:
 	$FogVolume2.show()
 	get_tree().create_tween().tween_property($FogVolume2, "size", Vector3(9,10.3,14), 1)
 
-func lastRoomTrigger(body: Node3D) -> void:
-	pass # Replace with function body.
+func lastRoomTrigger(_body: Node3D) -> void:
+	hud.get_node("lastRoomLabl").show()
+	await get_tree().create_timer(3).timeout
+	hud.get_node("lastRoomLabl").hide()
